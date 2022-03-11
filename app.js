3b190bec9f9e4260ee9558819388409f91d56fe1
Carl-Magnus Lein <carl@live.no>
2022-03-11 13:48:03 +0100

    Remove osmosis

--- app.js
+++ app.js
@@ -2,32 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var todaysDish = null;
-var price = "";
+const MongoClient = require('mongodb').MongoClient
 
-var osmosis = require('osmosis');
-osmosis
-.get('https://www.sio.no/mat-og-drikke/_window/mat+og+drikke+-+dagens+middag?s=284&_=1639827212459')
-.find('p')
-.set('body')
-.data(function(listing) {
-    console.log(listing)
-    if(todaysDish == null){
-      todaysDish = listing;
-    } else{
-      price = listing;
-    }
-})
-.log(console.log)
-.error(console.log)
+// MongoClient.connect('mongodb://localhost:27017/food', (err, client) => {
+//   if (err) throw err
 
-app.get('/', (req, res) => {
-  if(todaysDish){
-    res.json({todaysDish: todaysDish, price: price})
-  }else{
-    res.send("Error fetching data")
-  }
-})
+//   const db = client.db('food')
+
+//   db.collection('food').find().toArray((err, result) => {
+//     if (err) throw err
+//     console.log(result)
+//   })
+// })
+
+const URL = 'https://www.sio.no/mat-og-drikke/_window/mat+og+drikke+-+dagens+middag?s=284&_=1639827212459'
+const axios = require("axios");
+
+app.get('/', async (req, res) => {
+  const { data } = await axios.get(URL);
+  console.log(data)
+  res.send(data)
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
